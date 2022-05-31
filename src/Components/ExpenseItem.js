b5e79df961aa17e270411b682ca5c3ae7a1185a6
Hemant-Lohar <hemantlohar585@gmail.com>
2022-05-31 20:20:58 +0530

    ExpenseItem CSS Added

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 6, 1)
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 250.56
   return (
     <div className="expense-item">
-      <div>May 30th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$ 250</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>$ {expenseAmount}</div>
       </div>
     </div>
   );
