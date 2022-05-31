1ff2e7a0605aac97bb058c55daa688e3c48cfe41
Hemant-Lohar <hemantlohar585@gmail.com>
2022-05-31 16:45:47 +0530

    Added ExpenceItem Component

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>May 30th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$ 250</div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
