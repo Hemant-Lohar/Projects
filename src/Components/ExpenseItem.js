af22468c6e3f92a580aff8cfd50cf15f53501228
Hemant-Lohar <hemantlohar585@gmail.com>
2022-05-31 20:14:19 +0530

    hi

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <>
+    <div className="expense-item">
       <div>May 30th 2022</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$ 250</div>
+        <div className='expense-item__price'>$ 250</div>
       </div>
-    </>
+    </div>
   );
 }
 
