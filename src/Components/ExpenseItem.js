f9dc823db8fee20ff1d5b041f45f15f8357a6c86
Hemant-Lohar <hemantlohar585@gmail.com>
2022-05-31 20:35:47 +0530

    Added Props for Passing data dynamically

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,15 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 6, 1)
-    const expenseTitle = 'Car Insurance'
-    const expenseAmount = 250.56
+function ExpenseItem(props) {
+    
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>$ {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>$ {props.amount}</div>
       </div>
     </div>
   );
